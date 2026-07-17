105837fc0c358e8f3b1e86ccfa406aa219898c89
Greg Fraser <scouter.greg@outlook.com>
2026-07-16 19:55:32 -0700

    adding roles api

--- api/roles/index.js
+++ api/roles/index.js
@@ -1,9 +1,6 @@
 module.exports = async function (context, req) {
-  // You can add real logic later
   context.res = {
     status: 200,
-    body: {
-      roles: []
-    }
+    body: { roles: [] }
   };
 };
